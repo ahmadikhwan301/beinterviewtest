5b8c75826c3eca52c100243a74a2339d4ce1fc05
Ahmad Ikhwan <ikhwan@onewaysms.com>
2021-08-03 15:46:22 +0800

    UI setup

---
+++ lib/pages/mainPage.dart
@@ -0,0 +1,122 @@
+import 'package:flutter/material.dart';
+import 'package:bwinterviewtest/themes/theme.dart';
+import 'package:bwinterviewtest/themes/lightColor.dart';
+import 'package:bwinterviewtest/widgets/extensions.dart';
+import 'package:bwinterviewtest/widgets/titleText.dart';
+import 'package:bwinterviewtest/pages/shoppingCartPage.dart';
+import 'package:bwinterviewtest/pages/homePage.dart';
+import 'package:bwinterviewtest/widgets/BottomNavigationBar/bottomNavigationBar.dart';
+
+class MainPage extends StatefulWidget {
+  MainPage({Key key, this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MainPageState createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  bool isHomePageSelected = true;
+
+  Widget _title() {
+    return Container(
+        margin: AppTheme.padding,
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: <Widget>[
+            Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: <Widget>[
+                TitleText(
+                  text: isHomePageSelected ? 'Our' : 'Shopping',
+                  fontSize: 27,
+                  fontWeight: FontWeight.w400,
+                ),
+                TitleText(
+                  text: isHomePageSelected ? 'Products' : 'Cart',
+                  fontSize: 27,
+                  fontWeight: FontWeight.w700,
+                ),
+              ],
+            ),
+            Spacer(),
+            !isHomePageSelected
+                ? Container(
+                    padding: EdgeInsets.all(10),
+                    child: Icon(
+                      Icons.delete_outline,
+                      color: LightColor.orange,
+                    ),
+                  ).ripple(() {}, borderRadius: BorderRadius.all(Radius.circular(13)))
+                : SizedBox()
+          ],
+        ));
+  }
+
+  void onBottomIconPressed(int index) {
+    if (index == 0 || index == 1) {
+      setState(() {
+        isHomePageSelected = true;
+      });
+    } else {
+      setState(() {
+        isHomePageSelected = false;
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: Stack(
+          fit: StackFit.expand,
+          children: <Widget>[
+            SingleChildScrollView(
+              child: Container(
+                height: AppTheme.fullHeight(context) - 50,
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Color(0xfffbfbfb),
+                      Color(0xfff7f7f7),
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                  ),
+                ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    _title(),
+                    Expanded(
+                      child: AnimatedSwitcher(
+                        duration: Duration(milliseconds: 300),
+                        switchInCurve: Curves.easeInToLinear,
+                        switchOutCurve: Curves.easeOutBack,
+                        child: isHomePageSelected
+                            ? MyHomePage()
+                            : Align(
+                                alignment: Alignment.topCenter,
+                                child: ShoppingCartPage(),
+                              ),
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              child: CustomBottomNavigationBar(
+                onIconPresedCallback: onBottomIconPressed,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
